ef75561759eefac2c926bbbfd69a55a47ed1092f
kongsin <pansansou@gmail.com>
2015-12-13 03:40:25 +0700

    all algorithm work on json

--- analysysModel.php
+++ analysysModel.php
@@ -1,94 +1,74 @@
 <?php
-function __autoload($class_name)
-{
+
+function __autoload($class_name) {
     include $class_name . '.php';
 }
 
-class analysysModel extends database_manager
-{
+class analysysModel extends database_manager {
+
     private $tableName;
     private $returnValue;
     private $algorithm;
     private $userID;
+    private $json;
 
-    function __construct()
-    {
-        $this->algorithm = filter_input(INPUT_POST, "algorithm");
-        $this->tableName = filter_input(INPUT_POST, "tableName");
-        $this->userID = filter_input(INPUT_POST, "userid");
+    function __construct() {
+        $this->json = json_decode(json_decode(file_get_contents("php://input"), true)["AnalysysLoaderReq"], true);
+        $this->algorithm = $this->json["algorithm"];
+        $this->tableName = $this->json["tableName"];
+        $this->userID = $this->json["userid"];
     }
 
-    function analysysNow()
-    {
+    function analysysNow() {
         if ($this->connection()) {
             switch ($this->algorithm) {
                 case 0 :
-                    $cluster_class_count=filter_input(INPUT_POST,"class_count");
-                    $max_itetia=filter_input(INPUT_POST,"max_iteria");
-                    $seed=filter_input(INPUT_POST,"seed");
-                    $missing_value=filter_input(INPUT_POST,"missing_value");
+                    $cluster_class_count = $this->json["class_count"];
+                    $max_itetia = $this->json["max_iteria"];
+                    $seed = $this->json["seed"];
+                    $missing_value = $this->json["missing_value"];
 
                     $clus = new setUpModel();
-                    $result = $clus->loadClsterDataFormTable($this->tableName, $this->algorithm,$this->userID,$cluster_class_count,$max_itetia,$seed,$missing_value);
+                    $result = $clus->loadClsterDataFormTable($this->tableName, $this->algorithm, $this->userID, $cluster_class_count, $max_itetia, $seed, $missing_value);
                     $this->returnValue ['model'] = $result;
-                    $this->returnValue ['algorithm']= 0;
-                    $this->returnValue ['count']= $cluster_class_count;
+                    $this->returnValue ['algorithm'] = 0;
+                    $this->returnValue ['count'] = $cluster_class_count;
                     break;
                 case 1 :
                     $clus = new setUpModel();
-                    $binarySplit=filter_input(INPUT_POST,"binarySplit");
-                    $confidentFactor=filter_input(INPUT_POST,"confidentFactor");
-                    $minNumObj=filter_input(INPUT_POST,"minNumObj");
-                    $numFolds=filter_input(INPUT_POST,"numFolds");
-                    $reduceErrorPuning=filter_input(INPUT_POST,"reduceErrorPuning");
-                    $treeSeed=filter_input(INPUT_POST,"treeSeed");
-                    $subTree=filter_input(INPUT_POST,"subTree");
-                    $unPruned=filter_input(INPUT_POST,"unPruned");
-                    $useLaplace=filter_input(INPUT_POST,"useLaplace");
+                    $binarySplit = $this->json["binarySplit"];
+                    $confidentFactor = $this->json["confidentFactor"];
+                    $minNumObj = $this->json["minNumObj"];
+                    $numFolds = $this->json["numFolds"];
+                    $reduceErrorPuning = $this->json["reduceErrorPuning"];
+                    $treeSeed = $this->json["treeSeed"];
+                    $subTree = $this->json["subTree"];
+                    $unPruned = $this->json["unPruned"];
+                    $useLaplace = $this->json["useLaplace"];
 
 
                     $result = $clus->loadTreeDataFormTable(
-                        $this->tableName,
-                        $this->algorithm,
-                        $this->userID,
-                        $binarySplit,
-                        $confidentFactor,
-                        $minNumObj,
-                        $numFolds,
-                        $reduceErrorPuning,
-                        $treeSeed,
-                        $subTree,
-                        $unPruned,
-                        $useLaplace);
+                            $this->tableName, $this->algorithm, $this->userID, $binarySplit, $confidentFactor, $minNumObj, $numFolds, $reduceErrorPuning, $treeSeed, $subTree, $unPruned, $useLaplace);
 
 
                     $this->returnValue ['model'] = $result;
-                    $this->returnValue ['algorithm']= 1;
+                    $this->returnValue ['algorithm'] = 1;
                     break;
                 case 2 :
-                    $classindex=filter_input(INPUT_POST,"classindex");
-                    $delta=filter_input(INPUT_POST,"delta");
-                    $lowerBoundMinSupport=filter_input(INPUT_POST,"lowerBoundMinSupport");
-                    $minMetric=filter_input(INPUT_POST,"minMetric");
-                    $numRules=filter_input(INPUT_POST,"numRules");
-                    $significanceLevel=filter_input(INPUT_POST,"significanceLevel");
-                    $upperBoundMinSupport=filter_input(INPUT_POST,"upperBoundMinSupport");
+                    $classindex = $this->json["classindex"];
+                    $delta = $this->json["delta"];
+                    $lowerBoundMinSupport = $this->json["lowerBoundMinSupport"];
+                    $minMetric = $this->json["minMetric"];
+                    $numRules = $this->json["numRules"];
+                    $significanceLevel = $this->json["significanceLevel"];
+                    $upperBoundMinSupport = $this->json["upperBoundMinSupport"];
 
                     $clus = new setUpModel();
                     $result = $clus->loadAprioriDataFormTable(
-                        $this->tableName,
-                        $this->algorithm,
-                        $this->userID,
-                        $classindex,
-                        $delta,
-                        $lowerBoundMinSupport,
-                        $minMetric,
-                        $numRules,
-                        $significanceLevel,
-                        $upperBoundMinSupport);
+                            $this->tableName, $this->algorithm, $this->userID, $classindex, $delta, $lowerBoundMinSupport, $minMetric, $numRules, $significanceLevel, $upperBoundMinSupport);
 
                     $this->returnValue ['model'] = $result;
-                    $this->returnValue ['algorithm']= 2;
+                    $this->returnValue ['algorithm'] = 2;
                     break;
 
                 default :
