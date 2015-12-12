00d2e8f9cdee283396fd856f52a096d81f0639fb
kongsin <pansansou@gmail.com>
2015-12-13 03:40:41 +0700

    all algorithm work on json

--- getTreeModel.php
+++ getTreeModel.php
@@ -1,51 +1,51 @@
 <?php
+
 error_reporting(0);
+
 class getTreeModel {
 
     private $data;
     private $userid;
     private $param;
     private $part;
+    private $json;
 
-    function __construct()
-    {
-        $this->userid = filter_input(INPUT_POST, "userid");
-        $this->param = filter_input(INPUT_POST, "param");
+    function __construct() {
+        $this->json = json_decode(json_decode(file_get_contents("php://input"), true)["TreeModelReq"], true);
+        $this->userid = $this->json["userid"];
+        $this->param = $this->json["param"];
         $this->part = 'modelfile/' . $this->userid . '/';
         $this->loadDataByParam($this->param);
     }
 
-    function loadDataByParam($myparam)
-    {
+    function loadDataByParam($myparam) {
         $this->getContent();
         if ($myparam == "summary") {
             $this->getSummary();
         } else if ($myparam == "body") {
             $this->getBody();
-        }else if ($myparam == 'leave'){
-
-        }else if($myparam == 'full_data'){
-           $this->getFullData();
+        } else if ($myparam == 'leave') {
+            
+        } else if ($myparam == 'full_data') {
+            $this->getFullData();
         }
     }
 
-    function getFullData()
-    {
+    function getFullData() {
         echo json_encode($this->data);
     }
 
-    function getLeaveVal(){
+    function getLeaveVal() {
         $val = array();
         for ($i = $this->getLast($this->data); $i < sizeof($this->data); $i++) {
-            if (trim(split(":",$this->data[$i])[0]) == "Number of Leaves") {
+            if (trim(split(":", $this->data[$i])[0]) == "Number of Leaves") {
                 $val[] = $this->data[$i];
             }
         }
         echo json_encode($val);
     }
 
-    private function getContent()
-    {
+    private function getContent() {
         $hfile = fopen($this->part . "temp_model.txt", "r");
         while (!feof($hfile)) {
             $this->data [] = fgets($hfile);
@@ -53,8 +53,7 @@ class getTreeModel {
         fclose($hfile);
     }
 
-    function getSummary()
-    {
+    function getSummary() {
         $val = array();
         for ($i = $this->getLast($this->data); $i < sizeof($this->data); $i++) {
             if (trim($this->data[$i]) != "") {
@@ -64,7 +63,7 @@ class getTreeModel {
         echo json_encode($val);
     }
 
-    private function getBody(){
+    private function getBody() {
         $val = array();
         for ($i = 6; $i < $this->getLast($this->data); $i++) {
             if (trim($this->data[$i]) != "") {
@@ -74,11 +73,10 @@ class getTreeModel {
         echo json_encode($val);
     }
 
-    function getLast($sdata)
-    {
+    function getLast($sdata) {
         $last = 0;
         foreach ($sdata as $key => $value) {
-            if (str_split(trim($value),16)[0]=="Number of Leaves") {
+            if (str_split(trim($value), 16)[0] == "Number of Leaves") {
                 $last = $key;
             }
         }
@@ -86,4 +84,5 @@ class getTreeModel {
     }
 
 }
-$tree=new getTreeModel();
+
+$tree = new getTreeModel();
